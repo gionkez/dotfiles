5f0bf3b563b01b5a199482a14dcae54a29b841b7
Giorgio Della Roscia <59824966+dellarosciagiorgio@users.noreply.github.com>
2022-12-31 10:33:12 +0100

    c

--- apps/rofi/powermenu.sh
+++ apps/rofi/powermenu.sh
@@ -3,8 +3,8 @@
 chosen=$(printf "shutdown\nrestart\nlock" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
-	"shutdown") poweroff ;;
-	"restart") reboot ;;
-	"lock") xdg-screensaver lock ;;
-	*) exit 1 ;;
+	"shutdown") poweroff;;
+	"restart") reboot;;
+	"lock") xdg-screensaver lock;;
+	*) exit 1;;
 esac
