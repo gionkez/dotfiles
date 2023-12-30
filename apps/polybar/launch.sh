05b8abb413f79e8dd80729e7a1ccf195381dbbb9
dellarosciagiorgio <dellarosciagiorgio@gmail.com>
2023-12-30 23:00:45 +0100

    c

--- apps/polybar/launch.sh
+++ apps/polybar/launch.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
+pkill polybar
 polybar-msg cmd quit
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
-
-echo "polybar launched..."
+echo tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar simplebar 2>&1 | tee -a /tmp/polybar1.log & disown
