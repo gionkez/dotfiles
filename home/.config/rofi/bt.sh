3f7375c3ac2cd0888f782510eb91ded1afd06204
dellarosciagiorgio <dellarosciagiorgio@gmail.com>
2024-12-04 15:36:25 +0100

    c

--- home/.config/rofi/bt.sh
+++ home/.config/rofi/bt.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-chosen=$(printf "bluetooth connect mini\nbluetooth disconnect mini\nbluetooth connect p20i\nbluetooth disconnect p20i" | rofi -dmenu -i -theme-str '@import "bt.rasi"')
+chosen=$(printf "connect mini\ndisconnect mini\nconnect p20i\ndisconnect p20i" | rofi -dmenu -i -theme-str '@import "bt.rasi"')
 
 case "$chosen" in
-	"bluetooth connect mini") (bluetoothctl agent KeyboardOnly; bluetoothctl default-agent; bluetoothctl power on; bluetoothctl connect $device)
+	"connect mini") (bluetoothctl agent KeyboardOnly; bluetoothctl default-agent; bluetoothctl power on; bluetoothctl connect $device)
 	;;
-	"bluetooth disconnect mini") (bluetoothctl disconnect $device; bluetoothctl power off)
+	"disconnect mini") (bluetoothctl disconnect $device; bluetoothctl power off)
 	;;
-	"bluetooth connect p20i") (bluetoothctl agent KeyboardOnly; bluetoothctl default-agent; bluetoothctl power on; bluetoothctl connect $device)
+	"connect p20i") (bluetoothctl agent KeyboardOnly; bluetoothctl default-agent; bluetoothctl power on; bluetoothctl connect $device)
 	;;
-	"bluetooth disconnect p20i") (bluetoothctl disconnect $device; bluetoothctl power off)
+	"disconnect p20i") (bluetoothctl disconnect $device; bluetoothctl power off)
 	;;
 	*) exit 1;;
 esac
