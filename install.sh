bd475a1875d2ef6ddcb9ca7105eb4ab7f9e74b49
dellarosciagiorgio <dellarosciagiorgio@gmail.com>
2025-02-20 16:11:22 +0100

    c

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cp ./home/.bash_aliases ~
+cp ./home/.bashrc ~
+cp ./home/.gitconfig ~
+cp ./home/.xsessionrc ~
+cp ./home/.bash_aliases ~
+cp -r ./home/.config ~
+cp -r ./wallpapers/imgs ~/Pictures/wallpapers/
