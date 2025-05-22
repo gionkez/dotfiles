354abd450771d7fcf6a7c7301a447a267ac980c9
dellarosciagiorgio <dellarosciagiorgio@gmail.com>
2025-05-22 12:54:14 +0200

    c

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -2,10 +2,10 @@ local autogroup = vim.api.nvim_create_augroup
 local sussmanGroup = autogroup('DMS',{})
 local autocmd=vim.api.nvim_create_autocmd
 autocmd('VimResized',{
-    group=sussmanGroup,
-    callback = function()
-        local currentTab=vim.fn.tabpagenr()
-        vim.cmd("tabdo wincmd =")
-        vim.cmd("tabnext " .. currentTab)
-    end
+        group=sussmanGroup,
+        callback = function()
+            local currentTab=vim.fn.tabpagenr()
+            vim.cmd("tabdo wincmd =")
+            vim.cmd("tabnext " .. currentTab)
+        end
 })
--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,3 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv",{desc = 'Switch current line with the one below'})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv",{desc = 'Switch current  line with the one above'})
 vim.keymap.set("n", "n", "nzzzv",{desc = 'Find  next, center, and open any fold'})
-
