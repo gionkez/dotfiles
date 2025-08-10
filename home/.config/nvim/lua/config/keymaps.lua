cdc0b0dd077c0940c598ec05e5b2b0b4d9426365
gionkez <dellarosciagiorgio@gmail.com>
2025-08-10 18:32:20 +0200

    c

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,4 @@ local c = v.cmd
 k("v", "J", ":m '>+1<CR>gv=gv", {desc = 'Switch current line with the one below'})
 k("v", "K", ":m '<-2<CR>gv=gv", {desc = 'Switch current  line with the one above'})
 k({"n", "v"}, "x", '"_x')
+k("n", "ycc", "yygccp", {remap = true})
