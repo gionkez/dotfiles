34cf942aec6d1da2e5562c4eefa520e7496a7739
gionkez <dellarosciagiorgio@gmail.com>
2025-09-03 10:55:26 +0200

    c

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,10 +1,15 @@
 local c = vim.cmd
+local r = require
 
 c("filetype plugin indent on")
 c("syntax enable")
 c("set number")
 c("set ruler")
 c("set relativenumber")
+c("set syntax")
+c("set smartindent")
+c("set splitbelow")
+c("set splitright")
 c("set tabstop=4")
 c("set shiftwidth=4")
 c("set softtabstop=4")
@@ -17,8 +22,8 @@ c[[
 	imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jugp-prev' : '<S-Tab>'
 	smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 ]]
-require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/config/luasnip/"})
-require("luasnip").config.set_config({
+r("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/config/luasnip/"})
+r("luasnip").config.set_config({
 	enable_autosnippets = true,
 	store_selection_keys = "<Tab>",
 })
